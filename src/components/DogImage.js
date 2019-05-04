66995c9e87b006a0cdfba7cffa7c23ffda60f728
Abigail <abigailboyer1@gmail.com>
2019-05-04 18:49:30 -0500

    add props with breed img

--- src/components/DogImage.js
+++ src/components/DogImage.js
@@ -1,38 +1,5 @@
 import React from 'react';
-import img from '../images/logo.svg';
-import axios from 'axios';
 
-class DogImage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      url: "https://dog.ceo/api/breed/husky/images/random",
-      breed: "",
-      image: ""
-    };
-  }
-
-  componentDidMount(){
-    axios.get('https://dog.ceo/api/breed/husky/images/random')
-    .then(response => {
-      console.log(response.data);
-      this.setState({ image: response.data.message });
-      console.log(this.state)
-    })
-    .catch(error => {
-      console.log(error);
-    })
-  }
-
-  render() {
-    const { error, isLoaded, image } = this.state;
-
-    return (
-      <React.Fragment>
-
-      </React.Fragment>
-    );
-  }
-}
+const DogImage = props => <img src={props.image} alt={`${props.breed} photo`}  />;
 
 export default DogImage;
