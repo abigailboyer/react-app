7b52f8fd4c9981bb446154b5d1833f8eb7c397cd
Abigail <abigailboyer1@gmail.com>
2019-05-04 13:17:17 -0500

    add dogimage component

---
+++ src/components/DogImage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import img from '../images/logo.svg';
+
+class DogImage extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+
+
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default DogImage;
